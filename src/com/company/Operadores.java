67e9bead6fa36a7e08d8d625d8143539bb728124
PatoProgramador <spartan1171947@hotmail.com>
2023-03-23 15:23:25 -0500

    Operadores

---
+++ src/com/company/Operadores.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+        // aritmeticos
+        // + - / * %
+        int number1 = 5;
+        int number2 = 2;
+
+        int resultado = number1 + number2;
+        int resultado2 = number1 - number2;
+        int resultado3 = number1 * number2;
+        double resultado4 = number1 / number2;
+
+        //logicos, relacion, comparacion, booleanos
+        /*
+        >
+        >=
+        <
+        <=
+        ==
+        !=
+        && and
+        || or
+        !
+         */
+
+        // asignacion
+        /*
+        =
+        +=
+        -=
+        /=
+        *=
+        %=
+         */
+
+        // incremento
+        // ++
+
+        // decremento
+        // --
+
+        // concatenacion
+        // + con cadenas de texto
+    }
+}
