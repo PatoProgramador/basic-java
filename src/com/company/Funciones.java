a4c65f8e7ba1873ca8bb59329a317b22ddb38891
PatoProgramador <spartan1171947@hotmail.com>
2023-03-23 15:44:15 -0500

    Funciones

---
+++ src/com/company/Funciones.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Funciones {
+    public static void main(String[] args) {
+        // sin parametros
+        holaMundo();
+        // con parametros
+        holaMundo2("Pato");
+        // Devolviendo un tipo de dato especifico
+        String hola = devolverHolaMundo();
+        System.out.println(hola);
+    }
+    // Funcion sin parametros
+    private static void holaMundo() {
+        System.out.println("Hola mundo desde un método");
+    }
+    // Funcion con parametros
+    private static void holaMundo2(String name) {
+        System.out.println("Hola, " + name);
+    }
+    // Devolviendo un tipo de dato especifico
+    private static String devolverHolaMundo() {
+        return "Holis soy un String";
+    }
+}
