e4c503031f89dfedf1b141975a9863ab2bd40648
PatoProgramador <spartan1171947@hotmail.com>
2023-03-23 19:46:15 -0500

    Clases básicas

---
+++ src/com/company/Car.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Car {
+
+    // atributos
+    String color;
+    String fabricante;
+    String modelo;
+    Double peso;
+    Double largo;
+    Integer velocidad = 0;
+
+    // constructores
+    public Car(String color, String fabricante, String modelo, Double peso, Double largo) {
+        this.color = color;
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.peso = peso;
+        this.largo = largo;
+    }
+
+    // comportamiento
+    public void acelerar(Integer cantidad){
+        if(cantidad > 0 && cantidad <= 120){
+            this.velocidad += cantidad;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", fabricante='" + fabricante + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", peso=" + peso +
+                ", largo=" + largo +
+                ", velocidad=" + velocidad +
+                '}';
+    }
+}
