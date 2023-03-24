1c2744a51c8345a334192bfa3c0f767b6aa3e89f
PatoProgramador <spartan1171947@hotmail.com>
2023-03-23 19:31:22 -0500

    Break y continue

--- src/com/company/WhileLoop.java
+++ src/com/company/WhileLoop.java
@@ -6,7 +6,9 @@ public class WhileLoop {
         int count = 0;
         while(count <= 10){
             count++;
-            if(count == 6) break; //continue; salta el valor 6 y sigue iterando
+            if(count == 6)
+                break;
+            //continue; salta el valor 6 y sigue iterando
             // break encuentra el valor y para las iteraciones
             System.out.println("Holis " + count);
         }
