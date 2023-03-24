66ea647aeed2dd5edfbbd451ae504a8295250da5
PatoProgramador <spartan1171947@hotmail.com>
2023-03-23 20:38:30 -0500

    Polimorfismo

---
+++ src/com/company/clases/PolimorfismoMain.java
@@ -0,0 +1,16 @@
+package com.company.clases;
+
+public class PolimorfismoMain {
+    public static void main(String[] args) {
+        Car coche1 = new Car();
+
+        ElectricCar coche2 = new ElectricCar();
+
+        // polimorfismo
+        Car coche4 = new ElectricCar();
+
+        if (coche4 instanceof ElectricCar && coche4 instanceof Car) {
+            System.out.println("Si es un coche electrico e instancia del modelo padre");
+        }
+    }
+}
